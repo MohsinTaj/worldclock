45622782a5cc0284f973b8293991850313556105
MohsinTaj <taj4300021@cloud.neduet.edu.pk>
2023-10-07 15:09:30 +0500

    class commits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import WorldClocks from './components/WorldClocks';
-
+// import 
 function App() {
+  const [count,setCount]=useState(0)
+  const increment=()=>{
+    setCount(count+1)
+  }
+  const decrement=()=>{
+    if(count===0){
+setCount(0)
+    }
+    else{
+    setCount(count-1)
+    }
+  }
+  function reset(){
+    setCount(0)
+  }
   return (
     <div className="App">
-      <h1>World Clocks</h1>
+      <h1>Count </h1>
+<div>
+<button onClick={increment}>
+  increment 
+</button>
+<button onClick={decrement}>
+  decrement
+</button>
+<button onClick={reset} disabled={count === 0}>
+  reset
+</button>
+<div>
+  
+  count is {count}
+</div>
+
+</div>
       <WorldClocks />
     </div>
   );
